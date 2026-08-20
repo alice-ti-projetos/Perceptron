499408ac32e65eec932c8156749f3c07c68fc2f6
AliceAxOliveira <aliceaxavieroliveira@gmail.com>
2026-08-20 14:02:50 -0300

    corrigindo

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         float x1 = 0.0f;
         float x2 = 0.0f;
         float x3 = 0.0f;
+        float target = 0.0f;
 
         //pesos
         float w1 = 0.0f;
@@ -27,6 +28,8 @@ public class Main {
             x2 = input.nextFloat();
             System.out.println("x3");
             x3 = input.nextFloat();
+            System.out.println("target (saida esperada)");
+            target = input.nextFloat();
             System.out.println("w1");
             w1 = input.nextFloat();
             System.out.println("w2");
@@ -40,30 +43,29 @@ public class Main {
         }
 
 
-        float soma = (x1 * w1) + (x2  * w2) + (x3 * w3) * b;
-        int saida = (soma >= 0)? 1: 0;
-        float erro = soma - saida;
-        System.out.println(soma);
+        float soma = (x1 * w1) + (x2 * w2) + (x3 * w3) + b;
+        int saida = (soma >= 0) ? 1 : 0;
+        float erro = target - saida;
+        System.out.println("Soma: " + soma);
+        System.out.println("Saida: " + saida);
+        System.out.println("Erro: " + erro);
 
-        if (saida != x1 || saida != x2 || saida != x3) {
-            float valorErro = soma;
+        if (saida != target) {
 
-            valorErro -= x1;
-            System.out.println(valorErro);
             float novoW1 = w1 + taxaDeAprendizagem * erro * x1;
             float novoW2 = w2 + taxaDeAprendizagem * erro * x2;
             float novoW3 = w3 + taxaDeAprendizagem * erro * x3;
-
+            float novoB  = b  + taxaDeAprendizagem * erro;
 
             System.out.println("Novo W1: " + novoW1);
             System.out.println("Novo W2: " + novoW2);
             System.out.println("Novo W3: " + novoW3);
+            System.out.println("Novo B: " + novoB);
 
-            soma =  (b * novoW1) + (b* novoW2) + (b* novoW3);
-
+            soma = (x1 * novoW1) + (x2 * novoW2) + (x3 * novoW3) + novoB;
         }
 
-        System.out.println("Soma: " + soma);
+        System.out.println("Soma final: " + soma);
 
 //        System.out.println(x1);
 //        System.out.println(x2);
